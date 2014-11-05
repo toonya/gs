7e83eefe9410c1360f16d1fc68f186a13f4f941e
toonya <msc5762@me.com>
2014-11-05 18:30:21 +0800

    demo 1.0

--- js/main.js
+++ js/main.js
@@ -10,28 +10,4 @@
 	// .from('.button', 2, { opacity: 0, scale: .2, ease: Elastic.easeOut })
 	// .from('.love-star', 1, { opacity: 0, scale: .2, ease: Elastic.easeOut })
 	// .from('.title', 1, { backgroundPosition: '50% -100%', ease: Elastic.easeOut })
-
-	var tl = new TimelineLite();
-
-	tl
-	.call(function(){
-		$('.page').addClass('animation-play')
-	}, [], this, 0)
-
-	
-	// .call(function(){
-	// 	$('.light').addClass('animation-finish')
-	// }, [], this, 3)
-	// .call(function(){
-	// 	$('.boy, .girl').addClass('animation-play');
-	// }, [], this, 2)
-	// .call(function(){
-	// 	$('.boy, .girl').addClass('animation-finish');
-	// }, [], this, 3.5)
-	// .call(function(){
-	// 	$('.button').addClass('animation-play');
-	// }, [], this, 3.5)
-	// .call(function(){
-	// 	$('.button').addClass('animation-finish');
-	// }, [], this, 4.5)
 })(jQuery)
